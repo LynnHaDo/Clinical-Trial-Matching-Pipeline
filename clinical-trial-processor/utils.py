cbf615596e5a5189f39ff2ef6ef60e371f6525b5
Lynn Do <do24l@mtholyoke.edu>
2026-04-23 01:28:59 -0400

    Load model and infer on clinical trials

--- clinical-trial-processor/utils.py
+++ clinical-trial-processor/utils.py
@@ -1,4 +1,6 @@
+import torch
 from torch.nn.utils.rnn import pad_sequence
+from constants import NCBI_DATASET_VOCAB_KEYS
 
 def collate_fn(batch):
     """
@@ -16,3 +18,42 @@ def collate_fn(batch):
     mask = (padded_sentences != 0).byte()
         
     return padded_sentences, padded_tags, mask
+
+def prepare_sequence(seq, to_ix):
+    """
+    Converts a text string into a tensor of vocabulary IDs.
+    """
+    # Split text into tokens (in a real pipeline, use a proper tokenizer like spacy)
+    tokens = str(seq).split() 
+    idxs = [to_ix.get(w.lower(), to_ix[NCBI_DATASET_VOCAB_KEYS.UNKNOWN]) for w in tokens]
+    return torch.tensor(idxs, dtype=torch.long)
+
+def extract_entities(text, tags):
+    """
+    Parses tags back into actual phrases
+    """
+    tokens = str(text).split()
+    entities = []
+    current_entity = []
+    current_tag_type = None
+
+    for token, tag in zip(tokens, tags):
+        if tag.startswith('B-'):
+            # Save previous entity if it exists
+            if current_entity:
+                entities.append({'text': " ".join(current_entity), 'tag': current_tag_type})
+            current_entity = [token]
+            current_tag_type = tag.split('-')[1:] # e.g., ['Neg', 'Disease']
+            current_tag_type = "-".join(current_tag_type)
+        elif tag.startswith('I-') and current_entity:
+            current_entity.append(token)
+        else:
+            if current_entity:
+                entities.append({'text': " ".join(current_entity), 'tag': current_tag_type})
+                current_entity = []
+                current_tag_type = None
+                
+    if current_entity:
+        entities.append({'text': " ".join(current_entity), 'tag': current_tag_type})
+        
+    return entities
